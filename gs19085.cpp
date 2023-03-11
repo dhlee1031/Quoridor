f825e3b21d258b313cd08dbfba1d37bfe22b2558
dhlee1031 <56053565+dhlee1031@users.noreply.github.com>
2023-03-11 22:26:17 +0900

    Update gs19085.cpp

--- gs19085.cpp
+++ gs19085.cpp
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <unordered_map>
 #include <queue>
-#define N 3
-#define B 1
+#include <vector>
 
 using namespace std;
 typedef long long ll;
-unordered_map<ll, int> M;
+unordered_map<ll, int> M, P;
+unordered_map<int, ll> K;
 queue<int> Q;
+vector<vector<int> > V;
 
-ll coorblk, cnt;
-int p1bn, p2bn;
+ll coorblk, cnt, prev;
+int N, B;
+int p1bn, p2bn, ccnt;
 int px1, py1, px2, py2;
-int pbk[N + 1][N + 1];
+int pbk[6][6];
 int dx[4] = { -1,0,1,0 }, dy[4] = { 0,1,0,-1 };
 int px[4][2] = { {-1,-1}, {-1,0}, {0,0}, {0,-1} }, py[4][2] = { {-1,0}, {0,0}, {0,-1}, {-1,-1} };
+int turn, vst[10000000];
+char filename[128];
+FILE *in, *out;
 
-void make_coorblk(int x1, int y1, int x2, int y2)
+void make_coorblk(int k, int x1, int y1, int x2, int y2)
 {
-	coorblk = 0;	coorblk |= p1bn;
+	coorblk = k - 1;
+	coorblk <<= 3;	coorblk |= p1bn;
 	coorblk <<= 3;	coorblk |= p2bn;
 	coorblk <<= 3;	coorblk |= x1;
 	coorblk <<= 3;	coorblk |= y1;
@@ -37,23 +43,53 @@ void get_coorblk(void)
 {
 	ll temp = coorblk;
 
+	for (int i = 1; i < N; i++)
+		for (int j = 1; j < N; j++)
+			pbk[i][j] = 0;
 	int m = (N - 1)*(N - 1);
 	for (int i = 0; i < m; i++)
 		if (((ll)1 << i) & coorblk)
-			pbk[i % (N - 1) + 1][i / (N - 1)] = 1;	//세로
+			pbk[i / (N - 1) + 1][i % (N - 1) + 1] = 1;	//세로
 	for (int i = m; i < m * 2; i++)
 		if (((ll)1 << i) & coorblk)
-			pbk[i % (N - 1) + 1][i / (N - 1)] = 2;	//가로
+			pbk[(i - m) / (N - 1) + 1][(i - m) % (N - 1) + 1] = 2;	//가로
 
 	coorblk >>= 32;
 	py2 = coorblk & 7;	coorblk >>= 3;
-	px2 = coorblk & 7; coorblk >>= 3;
-	py1 = coorblk & 7; coorblk >>= 3;
-	px1 = coorblk & 7; coorblk >>= 3;
-	p2bn = coorblk & 7; coorblk >>= 3;
-	p1bn = coorblk & 7;
+	px2 = coorblk & 7;	coorblk >>= 3;
+	py1 = coorblk & 7;	coorblk >>= 3;
+	px1 = coorblk & 7;	coorblk >>= 3;
+	p2bn = coorblk & 7;	coorblk >>= 3;
+	p1bn = coorblk & 7; coorblk >>= 3;
+	turn = (coorblk & 1) + 1;
 
 	coorblk = temp;
+}
+void show_coorblk(void)
+{
+	get_coorblk();
+	int a[6][6] = { 0 };
+	a[N - py1 + 1][px1] = 1;
+	a[N - py2 + 1][px2] = 2;
+
+	fprintf(out, "whos turn? %d\n", turn);
+	fprintf(out, "player location:\n");
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+			fprintf(out, "%d ", a[i][j]);
+		fprintf(out, "\n");
+	}
+	fprintf(out, "block location:\n");
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 1; j < N; j++)
+			fprintf(out, "%d ", pbk[j][N - i]);
+		fprintf(out, "\n");
+	}
+	fprintf(out, "p1bn, p2bn:\n");
+	fprintf(out, "%d %d\n", p1bn, p2bn);
+	fprintf(out, "\n");
 }
 bool small_range(int x)
 {
@@ -67,23 +103,19 @@ bool large_range(int x)
 		return true;
 	return false;
 }
-bool can_go(int x, int y, int k)
-{
-	int x1 = x + px[k][0], y1 = y + py[k][0], x2 = x + px[k][1], y2 = y + py[k][1];
-
-	if (!(large_range(x + dx[k]) && large_range(y + dy[k])))
-		return false;
-	if (small_range(x1) && small_range(y1) && pbk[x1][y1] == k % 2 + 1)
-		return false;
-	if (small_range(x2) && small_range(y2) && pbk[x2][y2] == k % 2 + 1)
-		return false;
-	return true;
-}
-bool pbk_exist(int x, int y, int k)
+bool pbk_empty(int x, int y, int k)
 {
 	if (small_range(x) && small_range(y) && pbk[x][y] == k)
 		return false;
 	return true;
+}
+bool can_go(int x, int y, int k)
+{
+	int x1 = x + px[k][0], y1 = y + py[k][0], x2 = x + px[k][1], y2 = y + py[k][1];
+
+	if (large_range(x + dx[k]) && large_range(y + dy[k]) && pbk_empty(x1, y1, k % 2 + 1) && pbk_empty(x2, y2, k % 2 + 1))
+			return true;
+	return false;
 }
 bool blk_ok(int z, int x, int y, int d)
 {
@@ -91,126 +123,173 @@ bool blk_ok(int z, int x, int y, int d)
 
 	if (z == 1 && p1bn == 0 || z == 2 && p2bn == 0)
 		return false;
-	if (!(d == 1 && pbk[x][y] == 0 && pbk_exist(x, y - 1, 1) && pbk_exist(x, y + 1, 1)
-	   || d == 2 && pbk[x][y] == 0 && pbk_exist(x - 1, y, 2) && pbk_exist(x + 1, y, 2)))
+	if (!(d == 1 && pbk[x][y] == 0 && pbk_empty(x, y - 1, 1) && pbk_empty(x, y + 1, 1)
+	   || d == 2 && pbk[x][y] == 0 && pbk_empty(x - 1, y, 2) && pbk_empty(x + 1, y, 2)))
 		return false;
 
-	int i, end = 0, xx, yy, vst[N + 1][N + 1] = { 0 };
+	int i, end1 = 0, end2 = 0, xx, yy, vst[6][6] = { 0 };
+	pbk[x][y] = d;
 	while (!Q.empty())
 		Q.pop();
-	if (z == 1) { Q.push(px1); Q.push(py1); vst[px1][py1] = 1; }
-	else { Q.push(px2); Q.push(py2); vst[px2][py2] = 1; }
-
-	while (!Q.empty() && !end)
+	Q.push(px1); Q.push(py1);
+	vst[px1][py1] = 1;
+	while (!Q.empty() && !end1)
 	{
 		xx = Q.front(); Q.pop();
 		yy = Q.front(); Q.pop();
-		if (z == 1 && yy == N || z == 2 && yy == 1)
+		if (yy == N)
 		{
-			end = 1;
+			end1 = 1;
 			continue;
 		}
 		for (i = 0; i < 4; i++)
-			if (can_go(xx, yy, i) && vst[xx][yy] == 0) {
+			if (can_go(xx, yy, i) && vst[xx + dx[i]][yy + dy[i]] == 0) {
 				Q.push(xx + dx[i]); Q.push(yy + dy[i]); vst[xx + dx[i]][yy + dy[i]] = 1;
 			}
 	}
 
-	if (end)
+	for (int i = 0; i < 6; i++)
+		for (int j = 0; j < 6; j++)
+			vst[i][j] = 0;
+	while (!Q.empty())
+		Q.pop();
+	Q.push(px2); Q.push(py2);
+	vst[px2][py2] = 1;
+	while (!Q.empty() && !end2)
+	{
+		xx = Q.front(); Q.pop();
+		yy = Q.front(); Q.pop();
+		if (yy == 1)
+		{
+			end2 = 1;
+			continue;
+		}
+		for (i = 0; i < 4; i++)
+			if (can_go(xx, yy, i) && vst[xx + dx[i]][yy + dy[i]] == 0) {
+				Q.push(xx + dx[i]); Q.push(yy + dy[i]); vst[xx + dx[i]][yy + dy[i]] = 1;
+			}
+	}
+	pbk[x][y] = 0;
+
+	if (end1 && end2)
 		return true;
 	return false;
 }
 void f(int);
-int insert(int i, int y, int n, int k)
+int insert(ll prev, int i, int y, int n, int k)
 {
 	if (M.find(coorblk) == M.end())
+	{
+		P.insert(make_pair(coorblk, ++cnt));
+		K.insert(make_pair(cnt, coorblk));
+		V.push_back(vector<int>());
+		V[P[prev]].push_back(P[coorblk]);
 		if (y == n)
 		{
 			M.insert(make_pair(coorblk, k));
-			return 10;
+			return -10;
 		}
 		else
 		{
 			M.insert(make_pair(coorblk, 0));
 			f(3 - k);
 		}
+	}
+	else
+		V[P[prev]].push_back(P[coorblk]);
 	return i;
 }
 void f(int k)
 {
 	get_coorblk();
-	int i, j, tx1 = px1, ty1 = py1, tx2 = px2, ty2 = py2, t1bn = p1bn, t2bn = p2bn, tbk[N + 1][N + 1];
+
+	ll prev = coorblk;
+	int i, j, tx1 = px1, ty1 = py1, tx2 = px2, ty2 = py2, t1bn = p1bn, t2bn = p2bn, tbk[6][6];
 	for (i = 1; i < N; i++)
 		for (j = 1; j < N; j++)
 			tbk[i][j] = pbk[i][j];
 
 	if (k == 1)
 	{
-		for (i = 1; i < 5; i++)
-			if (can_go(tx1, ty1, i % 4))
+		for (i = 1; i < 5 && i > 0; i++)
+			if (i > 0 && can_go(tx1, ty1, i % 4))
 			{
-				if (tx1 + dx[i % 4] == tx2 && ty1 + dy[i % 4] == ty2)
+				if (i > 0 && tx1 + dx[i % 4] == tx2 && ty1 + dy[i % 4] == ty2)
 				{
-					if (can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], i % 4))
+					if (i > 0 && can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], i % 4))
 					{
-						make_coorblk(tx1 + 2 * dx[i % 4], ty1 + 2 * dy[i % 4], tx2, ty2);
-						i = insert(i, ty1 + 2 * dy[i % 4], N, k);
+						make_coorblk(3 - k, tx1 + 2 * dx[i % 4], ty1 + 2 * dy[i % 4], tx2, ty2);
+						i = insert(prev, i, ty1 + 2 * dy[i % 4], N, k);
+						make_coorblk(k, tx1, ty1, tx2, ty2);
 					}
-					else
+					else if (i > 0)
 					{
-						if (can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], (i + 1) % 4))
+						if (i > 0 && can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], (i + 1) % 4))
 						{
-							make_coorblk(tx1 + dx[i % 4] + dx[(i + 1) % 4], ty1 + dy[i % 4] + dy[(i + 1) % 4], tx2, ty2);
-							i = insert(i, ty1 + dy[i % 4] + dy[(i + 1) % 4], N, k);
+							make_coorblk(3 - k, tx1 + dx[i % 4] + dx[(i + 1) % 4], ty1 + dy[i % 4] + dy[(i + 1) % 4], tx2, ty2);
+							i = insert(prev, i, ty1 + dy[i % 4] + dy[(i + 1) % 4], N, k);
+							make_coorblk(k, tx1, ty1, tx2, ty2);
 						}
-						if (can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], (i + 3) % 4))
+						if (i > 0 && can_go(tx1 + dx[i % 4], ty1 + dy[i % 4], (i + 3) % 4))
 						{
-							make_coorblk(tx1 + dx[i % 4] + dx[(i + 3) % 4], ty1 + dy[i % 4] + dy[(i + 3) % 4], tx2, ty2);
-							i = insert(i, ty1 + dy[i % 4] + dy[(i + 3) % 4], N, k);
+							make_coorblk(3 - k, tx1 + dx[i % 4] + dx[(i + 3) % 4], ty1 + dy[i % 4] + dy[(i + 3) % 4], tx2, ty2);
+							i = insert(prev, i, ty1 + dy[i % 4] + dy[(i + 3) % 4], N, k);
+							make_coorblk(k, tx1, ty1, tx2, ty2);
 						}
 					}
 				}
 				else
 				{
-					make_coorblk(tx1 + dx[i % 4], ty1 + dy[i % 4], tx2, ty2);
-					i = insert(i, ty1 + dy[i % 4], N, k);
+					if (i > 0)
+					{
+						make_coorblk(3 - k, tx1 + dx[i % 4], ty1 + dy[i % 4], tx2, ty2);
+						i = insert(prev, i, ty1 + dy[i % 4], N, k);
+						make_coorblk(k, tx1, ty1, tx2, ty2);
+					}
 				}
 			}
 	}
 	else
 	{
-		for (i = 3; i < 7; i++)
-			if (can_go(tx2, ty2, i % 4))
+		for (i = 3; i < 7 && i > 0; i++)
+			if (i > 0 && can_go(tx2, ty2, i % 4))
 			{
-				if (tx2 + dx[i % 4] == tx1 && ty2 + dy[i % 4] == ty1)
+				if (i > 0 && tx2 + dx[i % 4] == tx1 && ty2 + dy[i % 4] == ty1)
 				{
-					if (can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], i % 4))
+					if (i > 0 && can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], i % 4))
 					{
-						make_coorblk(tx1, ty1, tx2 + 2 * dx[i % 4], ty2 + 2 * dy[i % 4]);
-						i = insert(i, ty2 + 2 * dy[i % 4], 1, k);
+						make_coorblk(3 - k, tx1, ty1, tx2 + 2 * dx[i % 4], ty2 + 2 * dy[i % 4]);
+						i = insert(prev, i, ty2 + 2 * dy[i % 4], 1, k);
+						make_coorblk(k, tx1, ty1, tx2, ty2);
 					}
-					else
+					else if (i > 0)
 					{
-						if (can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], (i + 1) % 4))
+						if (i > 0 && can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], (i + 1) % 4))
 						{
-							make_coorblk(tx1, ty1, tx2 + dx[i % 4] + dx[(i + 1) % 4], ty2 + dy[i % 4] + dy[(i + 1) % 4]);
-							i = insert(i, ty2 + dy[i % 4] + dy[(i + 1) % 4], 1, k);
+							make_coorblk(3 - k, tx1, ty1, tx2 + dx[i % 4] + dx[(i + 1) % 4], ty2 + dy[i % 4] + dy[(i + 1) % 4]);
+							i = insert(prev, i, ty2 + dy[i % 4] + dy[(i + 1) % 4], 1, k);
+							make_coorblk(k, tx1, ty1, tx2, ty2);
 						}
-						if (can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], (i + 3) % 4))
+						if (i > 0 && can_go(tx2 + dx[i % 4], ty2 + dy[i % 4], (i + 3) % 4))
 						{
-							make_coorblk(tx1, ty1, tx2 + dx[i % 4] + dx[(i + 3) % 4], ty2 + dy[i % 4] + dy[(i + 3) % 4]);
-							i = insert(i, ty2 + dy[i % 4] + dy[(i + 3) % 4], 1, k);
+							make_coorblk(3 - k, tx1, ty1, tx2 + dx[i % 4] + dx[(i + 3) % 4], ty2 + dy[i % 4] + dy[(i + 3) % 4]);
+							i = insert(prev, i, ty2 + dy[i % 4] + dy[(i + 3) % 4], 1, k);
+							make_coorblk(k, tx1, ty1, tx2, ty2);
 						}
 					}
 				}
 				else
 				{
-					make_coorblk(tx1, ty1, tx2 + dx[i % 4], ty2 + dy[i % 4]);
-					i = insert(i, ty2 + dy[i % 4], 1, k);
+					if (i > 0)
+					{
+						make_coorblk(3 - k, tx1, ty1, tx2 + dx[i % 4], ty2 + dy[i % 4]);
+						i = insert(prev, i, ty2 + dy[i % 4], 1, k);
+						make_coorblk(k, tx1, ty1, tx2, ty2);
+					}
 				}
 			}
 	}
-	if (i != 10)
+	if (i > 0)
 	{
 		int t = 0;
 		for (int l = 1; l <= 2; l++)
@@ -218,60 +297,84 @@ void f(int k)
 				for (int j = 1; j < N; j++)
 					if (blk_ok(k, i, j, l))
 					{
-						
-						pbk[i][j] = l;
+						tbk[i][j] = l;
 						if (k == 1) t1bn--;
 						else t2bn--;
 						p1bn = t1bn; p2bn = t2bn;
-						make_coorblk(tx1, ty1, tx2, ty2);
+						for (int a = 1; a < N; a++)
+							for (int b = 1; b < N; b++)
+								pbk[a][b] = tbk[a][b];
+						make_coorblk(3 - k, tx1, ty1, tx2, ty2);
 
-						t = insert(0, 0, 1, k);
+						t = insert(prev, 0, 0, 1, k);
 
-						pbk[i][j] = 0;
+						tbk[i][j] = 0;
 						if (k == 1) t1bn++;
 						else t2bn++;
 						p1bn = t1bn; p2bn = t2bn;
-						make_coorblk(tx1, ty1, tx2, ty2);
+						for (int a = 1; a < N; a++)
+							for (int b = 1; b < N; b++)
+								pbk[a][b] = tbk[a][b];
+						make_coorblk(k, tx1, ty1, tx2, ty2);
 					}
 	}
 }
+void dfs(int idx)
+{
+	vst[idx] = 1;
+	coorblk = K[idx];
+	fprintf(out, "%d\n", idx);
+	show_coorblk();
+
+	int siz = V[idx].size();
+	for (int i = 0; i < siz; i++)
+		if (vst[V[idx][i]] == 0)
+			dfs(V[idx][i]);
+}
 int main(void)
 {
+	in = fopen("input.txt", "r");
+	fscanf(in, "%d %d", &N, &B);
+
 	p1bn = p2bn = B;
-	make_coorblk(N / 2 + 1, 1, N / 2 + 1, N);
+	make_coorblk(1, N / 2 + 1, 1, N / 2 + 1, N);
 	M.insert(make_pair(coorblk, 0));
+	P.insert(make_pair(coorblk, ++cnt));
+	K.insert(make_pair(cnt, coorblk));
+	V.push_back(vector<int>());
+	V.push_back(vector<int>());
 	f(1);
 
-	FILE *out = fopen("out.txt", "w");
+	printf("\n");
+	sprintf(filename, "%dx%d_%d.txt", N, N, B);
+	out = fopen(filename, "w");
+	fprintf(out, "\n");
+	fclose(out);
+	out = fopen(filename, "a");
+	fprintf(out, "%lld\n", M.size());
 
 	auto iter = M.begin();
 	for (; iter != M.end(); ++iter)
 	{
-		cnt++;
 		coorblk = iter->first;
-		get_coorblk();
-		int a[N + 1][N + 1] = { 0 };
-		a[N - py1 + 1][px1] = 1;
-		a[N - py2 + 1][px2] = 2;
-		if (pbk[1][1] + pbk[1][2] + pbk[2][1] + pbk[2][2] == 0)
-		{
-			for (int i = 1; i <= N; i++)
-			{
-				for (int j = 1; j <= N; j++)
-					fprintf(out, "%d ", a[i][j]);
-				fprintf(out, "\n");
-			}
-			for (int i = 1; i < N; i++)
-			{
-				for (int j = 1; j < N; j++)
-					fprintf(out, "%d ", pbk[i][j]);
-				fprintf(out, "\n");
-			}
-			fprintf(out, "\n");
-		}
+//		show_coorblk();
+		fprintf(out, "%d %lld\n", P[coorblk], coorblk);
 	}
-	fprintf(out, "%lld\n", cnt);
-	
+	printf("%lld\n", cnt);
+	for (int i = 1; i <= cnt; i++)
+	{
+		int siz = V[i].size();
+		fprintf(out, "%d ", i);
+		fprintf(out, "%d ", V[i].size());
+		for (int j = 0; j < siz; j++)
+			fprintf(out, "%d ", V[i][j]);
+		fprintf(out, "\n");
+	}
+	fclose(out);
+
+	out = fopen("show_dfs.txt", "w");
+	dfs(1);
+	fclose(out);
 
 	return 0;
 }
